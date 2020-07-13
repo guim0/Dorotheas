d05d84566177d6b9d3862b756f779105a2f95205
oaparecido1 <daniel.aparecido4683@gmail.com>
2020-07-13 10:54:48 -0300

    database config and migrations created

---
+++ database/migrations/2020_07_13_122120_create_teacher_course_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTeacherCourseTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teacher_course', function (Blueprint $table) {
+            $table->integer('id_course');
+            $table->integer('id_teacher');
+            $table->string('period');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teacher_course');
+    }
+}
