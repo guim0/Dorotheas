6ca8be043e6884d4648891aaebdd42b9cfe6574e
oaparecido1 <daniel.aparecido4683@gmail.com>
2020-07-13 15:15:14 -0300

    Adding controller student and blade student

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class StudentController
+{
+    public function index()
+    {
+        return view('student.all');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'IndexController@index');
-
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Routes
+
+Route::get('/', 'IndexController@index')->name('site.home');
+
+Route::get('/students', 'StudentController@index')->name('site.students');
+
+Route::get('/teachers', 'TeacherController@index')->name('site.teachers');
+
+Route::get('/courses', 'CoursesController@index')->name('site.courses');
