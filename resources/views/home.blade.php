ccf2ee14370c589834efd9b873ca818e7ebbb45b
Aparecido <aparecido@pop-os.localdomain>
2020-07-21 20:02:34 -0300

    view home, scroll

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,15 @@
                 <img src="{!! asset('images/HomePage/ballet.jpg') !!}" alt="">
                 <img src="{!! asset('images/HomePage/english.png') !!}" alt="">
                 <img src="{!! asset('images/HomePage/taekwondo.webp') !!}" alt="">
+                <img src="{!! asset('images/HomePage/ballet.jpg') !!}" alt="">
+                <img src="{!! asset('images/HomePage/english.png') !!}" alt="">
+                <img src="{!! asset('images/HomePage/taekwondo.webp') !!}" alt="">
+                <img src="{!! asset('images/HomePage/ballet.jpg') !!}" alt="">
+                <img src="{!! asset('images/HomePage/english.png') !!}" alt="">
+                <img src="{!! asset('images/HomePage/taekwondo.webp') !!}" alt="">
+                <img src="{!! asset('images/HomePage/ballet.jpg') !!}" alt="">
+                <img src="{!! asset('images/HomePage/english.png') !!}" alt="">
+                <img src="{!! asset('images/HomePage/taekwondo.webp') !!}" alt="">
             </div>
         </div>
         <div class="mysite_home-menu d-flex flex-column align-items-center justify-content-around">
@@ -15,9 +24,11 @@
                 <hr>
             </div>
             <div class="mysite_home-card-teachers d-flex">
-                <div class="mysite_home-img d-flex align-items-center justify-content-center">
-                    <img src="{!! asset('images/HomePage/teacher.svg') !!}" alt="">
-                </div>
+                <a href="#">
+                    <div class="mysite_home-img d-flex align-items-center justify-content-center">
+                        <img src="{!! asset('images/HomePage/teacher.svg') !!}" alt="">
+                    </div>
+                </a>
                 <div class="mysite_home-content-card ml-2">
                     <h2>Professores</h2>
                     <div class="mysite_home-content-teacher d-flex ml-3">
@@ -32,34 +43,38 @@
             </div>
 
             <div class="mysite_home-card-teachers d-flex">
-                <div class="mysite_home-img d-flex align-items-center justify-content-center">
-                    <img src="{!! asset('images/HomePage/teacher.svg') !!}" alt="">
-                </div>
+                <a href="">
+                    <div class="mysite_home-img d-flex align-items-center justify-content-center">
+                        <img src="{!! asset('images/HomePage/student.png') !!}" alt="">
+                    </div>
+                </a>
                 <div class="mysite_home-content-card ml-2">
-                    <h2>Professores</h2>
+                    <h2>Alunos</h2>
                     <div class="mysite_home-content-teacher d-flex ml-3">
                         <img src="{!! asset('images/HomePage/quote.png') !!}" alt="Quote">
                         <p>
-                            Educar é impregnar de sentido o que fazemos a cada instante!
+                            A vida é uma grande universidade, mas pouco ensina a quem não sabe ser um aluno...
                             <br>
-                            - Paulo Freire
+                            - Augusto Cury
                         </p>
                     </div>
                 </div>
             </div>
 
             <div class="mysite_home-card-teachers d-flex">
-                <div class="mysite_home-img d-flex align-items-center justify-content-center">
-                    <img src="{!! asset('images/HomePage/teacher.svg') !!}" alt="">
-                </div>
+                <a href="">
+                    <div class="mysite_home-img d-flex align-items-center justify-content-center">
+                        <img src="{!! asset('images/HomePage/book.png') !!}" alt="">
+                    </div>
+                </a>
                 <div class="mysite_home-content-card ml-2">
-                    <h2>Professores</h2>
+                    <h2>Matérias</h2>
                     <div class="mysite_home-content-teacher d-flex ml-3">
                         <img src="{!! asset('images/HomePage/quote.png') !!}" alt="Quote">
                         <p>
-                            Educar é impregnar de sentido o que fazemos a cada instante!
+                            A alegria que se tem em pensar e aprender faz-nos pensar e aprender ainda mais.
                             <br>
-                            - Paulo Freire
+                            - Aristóteles
                         </p>
                     </div>
                 </div>
--- resources/views/student/all.blade.php
+++ resources/views/student/all.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.main')
+@extends('layouts.student')
 
 @section('content')
     <div class="container">
